593ffc8033f376b9d9b1542f2a8c205c99a0cb7c
jarvondigital <jarvondigital@gmail.com>
2025-09-08 15:54:26 -0400

    - Added Users

--- src/app/components/user-profile/user-profile.ts
+++ src/app/components/user-profile/user-profile.ts
@@ -1,15 +1,29 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {Avatar} from 'primeng/avatar';
 import {UserEngine} from '../../services/user.engine';
+import {AvatarGroup} from 'primeng/avatargroup';
+import {AsyncPipe, DatePipe} from '@angular/common';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {from} from 'rxjs';
+import {Divider} from 'primeng/divider';
+import {Tooltip} from 'primeng/tooltip';
 
 @Component({
   selector: 'app-user-profile',
   imports: [
-    Avatar
+    Avatar,
+    AvatarGroup,
+    AsyncPipe,
+    Divider,
+    DatePipe,
+    Tooltip
   ],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.scss'
 })
-export class UserProfile {
+export class UserProfile{
   user: UserEngine = inject(UserEngine);
+  $users = toSignal(from(this.user.getUsers()))
+
+
 }
--- src/app/services/user.engine.ts
+++ src/app/services/user.engine.ts
@@ -5,8 +5,9 @@ import {
   setPersistence,
   browserLocalPersistence, user, signOut, User
 } from '@angular/fire/auth';
-import {firstValueFrom, from} from 'rxjs';
+import {firstValueFrom, from, map} from 'rxjs';
 import {toSignal} from '@angular/core/rxjs-interop';
+import {addDoc, collection, doc, Firestore, getDoc, getDocs, setDoc} from '@angular/fire/firestore';
 
 export interface UserLoginForm {
   email: string;
@@ -19,10 +20,49 @@ export interface UserLoginForm {
 export class UserEngine {
 
   auth = inject(Auth)
+  firestore = inject(Firestore)
+  userCollection = collection(this.firestore, 'users');
   $signedInUser: Signal<User | null | undefined> = toSignal(from(user(this.auth)));
 
-  isUserSignedIn() {
-    return user(this.auth)
+  /**
+   * Effect function to update the user database with the signed-in user's information.
+   *
+   * This function checks if a user is signed in and fetches the user's document
+   * reference in the `userCollection`. If the user's document does not exist in the
+   * database, the function creates a new document with the user's details, including
+   * UID, email, display name, photo URL, email verification status, account creation time,
+   * and last login time.
+   *
+   * The function performs the following:
+   * - Verifies if a user is signed in.
+   * - Retrieves the document reference for the signed-in user from the `userCollection`.
+   * - Checks if the user's document exists in the database.
+   * - Creates a new document with the user's details if it does not exist.
+   *
+   * This operation is asynchronous.
+   */
+  updateUserDB = effect(async () => {
+    const user = this.$signedInUser();
+    if(!user) return;
+
+    const docRef = doc(this.userCollection, user.uid);
+    const docData = await getDoc(docRef);
+    if(!docData.exists()) {
+      await setDoc(docRef, {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+        emailVerified: user.emailVerified,
+        createdAt: user.metadata.creationTime,
+        lastLogin: user.metadata.lastSignInTime
+      })
+    }
+  })
+
+  async getUsers(): Promise<User[]> {
+    let data = await getDocs(this.userCollection)
+    return data.docs.map(d => d.data() as User)
   }
 
   async loginWithEmailAndPassword({email, password}: UserLoginForm) {
